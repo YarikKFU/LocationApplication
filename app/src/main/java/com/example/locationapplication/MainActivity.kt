dd2b9dff454557547c107e77143ec4c8d22e1c28
marsel <blohin.yarik@yandex.ru>
2021-05-20 18:41:03 +0300

    Разрешение на Локацию

--- app/src/main/java/com/example/locationapplication/MainActivity.kt
+++ app/src/main/java/com/example/locationapplication/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.locationapplication
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                requestLocationPermission()
+        }
+        btn_stop.setOnClickListener {}
     }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun requestLocationPermission() {
+        this.requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 101)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
 }
